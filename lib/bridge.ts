1d949ac5d5dc8c66480b42a2c1c348015074cfa3
Alex Yuly <alexyuly2@gmail.com>
2023-10-10 21:57:18 -0700

    v0.3.3

--- lib/bridge.ts
+++ lib/bridge.ts
@@ -306,7 +306,7 @@ export function render(rootElement: Abstract.Element): void;
 export function render(root: Abstract.View): void;
 export function render(argument: Abstract.Element | Abstract.View): void {
   const app: Abstract.App = {
-    kind: "ViewScript v0.3.2 App",
+    kind: "ViewScript v0.3.3 App",
     root: Abstract.isElement(argument)
       ? {
           kind: "view",
